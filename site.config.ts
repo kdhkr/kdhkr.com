7786a8d2a1eab9fe405cca90830be3afa73f7620
Do Hoon Kim <op@kdh.io>
2023-08-29 17:52:45 -1000

    Update

--- site.config.ts
+++ site.config.ts
@@ -54,6 +54,14 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [
     {
+      title: 'Home',
+      pageId: '1a1a5809315e4392b8d0f41250d0961a'
+    },
+    {
+      title: 'Notice',
+      pageId: 'b602430c53fb469bbe046c3cfd238775'
+    },
+    {
       title: 'About',
       pageId: '50744fe6fac04c66b08084c4de05e30c'
     },
@@ -64,10 +72,6 @@ export default siteConfig({
     {
       title: 'Activities',
       pageId: '8ef7e5ea33e340fba7de5b2e32d14049'
-    },
-    {
-      title: 'Notice',
-      pageId: 'b602430c53fb469bbe046c3cfd238775'
     },
     {
       title: 'Blog',
