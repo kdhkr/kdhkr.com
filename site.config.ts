d21a553c37124a24b9d4f3662da7dbc4ad18b966
Do Hoon Kim <op@kdh.io>
2023-08-29 17:49:54 -1000

    Update

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,28 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '50744fe6fac04c66b08084c4de05e30c'
+    },
+    {
+      title: 'Projects',
+      pageId: '3c44d6f063514032a0de9dd8281897ab'
+    },
+    {
+      title: 'Activities',
+      pageId: '8ef7e5ea33e340fba7de5b2e32d14049'
+    },
+    {
+      title: 'Notice',
+      pageId: 'b602430c53fb469bbe046c3cfd238775'
+    },
+    {
+      title: 'Blog',
+      pageId: '47846ada1e874077aa9181fa8d0c165f'
+    }
+  ]
 })
